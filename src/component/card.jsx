e2cab0c8a42158fabf3d5eab060de129b2717b9b
hauzankhoirullah <hauzan41200@gmail.com>
2023-08-26 20:07:05 +0700

    add responsive and integration

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -6,16 +6,16 @@ function Card({title, banner, genre}) {
     <>
         <div className='relative '>
             <div className='group bg-black rounded-md'>
-                <img className='shadow-xl h-96 group-hover:opacity-25 hover:opacity-25' src={banner} alt="movie-banner" />
-                <div className=''>
-                    <div className='invisible group-hover:visible flex flex-col absolute top-32 left-10'>
-                        <button className=' btn bg-inherit text-white hover:text-slate-500 border-2 w-44 capitalize mb-5'>Details</button>
-                        <button className='bg-blue-700 hover:bg-white hover:text-blue-700 text-white font-bold py-3 px-4 rounded-lg w-44 capitalize'>Buy Tickets</button>
+                <img className='shadow-xl h-64 w-full sm:h-96 group-hover:opacity-25 hover:opacity-25 object-center' src={banner} alt="movie-banner" />
+
+                    <div className='invisible group-hover:visible flex flex-col absolute top-32 left-0 right-0 mx-auto justify-center items-center'>
+                        <button className=' btn bg-inherit text-white hover:text-slate-500 border-2 w-20 md:w-44 capitalize mb-5'>Details</button>
+                        <button className='bg-blue-700 hover:bg-white hover:text-blue-700 text-white font-bold py-3 px-4 rounded-lg text w-20 md:w-44 capitalize'>Buy Tickets</button>
                     </div>
-                </div>
+                
             </div>
             <div className='pt-8'>
-                <span className='text-xl font-medium'>{title}</span>
+                <span className=' md:text-lg lg:text-xl font-medium '>{title}</span>
                 <div className='grid grid-cols-2 gap-2'>
                     <span className='btn rounded-full text-slate-400 cursor-default capitalize mt-5'>{genre[0]}</span>
                     {
